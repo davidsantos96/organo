4384f660d7bb3a7023fe879ee225878caa97cb13
David Patricio <davidpbarros96@gmail.com>
2025-07-22 15:20:52 -0300

    adicionado função de deletar colaborador

--- src/App.js
+++ src/App.js
@@ -3,38 +3,88 @@ import Formulario from './componentes/Formulario';
 import { useState } from 'react';
 import Time from './componentes/Time';
 import Rodape from './componentes/Rodape';
+import { v4 as uuidv4 } from 'uuid';
+
+
 
 
 
 function App() {
 
-  const times = [
-    { nome: 'Programação', corPrimaria: '#57C278', corSecundaria: '#D9F7E9' },
-    { nome: 'Front-End', corPrimaria: '#82CFFA', corSecundaria: '#E8F8FF' },
-    { nome: 'Data Science', corPrimaria: '#A6D157', corSecundaria: '#F0F8E2' },
-    { nome: 'Devops', corPrimaria: '#E06B69', corSecundaria: '#FDE7E8' },
-    { nome: 'UX e Design', corPrimaria: '#DB6EBF', corSecundaria: '#FAE9F5' },
-    { nome: 'Mobile', corPrimaria: '#FFBA05', corSecundaria: '#FFF5D9' },
-    { nome: 'Inovação e Gestão', corPrimaria: '#FF8A29', corSecundaria: '#FFEEDF' }
-  ]
+   const [times, setTimes] = useState([
+    { id: uuidv4(), nome: 'Programação', corPrimaria: '#57C278', corSecundaria: '#D9F7E9' },
+    { id: uuidv4(), nome: 'Front-End', corPrimaria: '#82CFFA', corSecundaria: '#E8F8FF' },
+    { id: uuidv4(), nome: 'Data Science', corPrimaria: '#A6D157', corSecundaria: '#F0F8E2' },
+    { id: uuidv4(), nome: 'Devops', corPrimaria: '#E06B69', corSecundaria: '#FDE7E8' },
+    { id: uuidv4(), nome: 'UX e Design', corPrimaria: '#DB6EBF', corSecundaria: '#FAE9F5' },
+    { id: uuidv4(), nome: 'Mobile', corPrimaria: '#FFBA05', corSecundaria: '#FFF5D9' },
+    { id: uuidv4(), nome: 'Inovação e Gestão', corPrimaria: '#FF8A29', corSecundaria: '#FFEEDF' }
+  ])
 
   const [colaboradores, setColaboradores] = useState([]);
     debugger
   const aoNovoColaboradorAdcionado = (colaborador) => {
-    setColaboradores([...colaboradores, colaborador]);
-
+    setColaboradores([...colaboradores, { ...colaborador, id: uuidv4() }]);
   }
+
+  function deletarColaborador(id) {  // ...existing code...
+  
+  function App() {
+  
+    const [times, setTimes] = useState([
+      // ...times...
+    ]);
+  
+    const [colaboradores, setColaboradores] = useState([]);
+  
+    const aoNovoColaboradorAdcionado = (colaborador) => {
+      setColaboradores([...colaboradores, { ...colaborador, id: uuidv4() }]);
+    };
+  
+    function deletarColaborador(id) {
+      setColaboradores(colaboradores.filter(colaborador => colaborador.id !== id));
+    }
+  
+    // O return deve estar dentro da função App!
+    return (
+      <div className="App">
+        <Banner />
+        <Formulario
+          times={times.map(time => time.nome)}
+          aoColaboradorCadastrado={aoNovoColaboradorAdcionado}
+        />
+        {times.map((time) => (
+          <Time
+            key={time.id}
+            nome={time.nome}
+            corPrimaria={time.corPrimaria}
+            corSecundaria={time.corSecundaria}
+            colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
+            aoDeletar={deletarColaborador}
+          />
+        ))}
+        <Rodape />
+      </div>
+    );
+  }
+  
+
+  // ...existing code...
+    setColaboradores(colaboradores.filter(colaborador => colaborador.id !== id));
+  }
+
   return (
     <div className="App">
       <Banner />
       <Formulario times={times.map(time => time.nome)} aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdcionado(colaborador)} />
 
-      {times.map(time => <Time 
-      nome={time.nome} 
-      key={time.nome} 
+      {times.map((time, indice) => <Time 
+      key={time.id}
+      nome={time.nome}
       corPrimaria={time.corPrimaria} 
       corSecundaria={time.corSecundaria}
       colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
+      aoDeletar={deletarColaborador}
       />
       )}
       <Rodape />
--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,18 +1,24 @@
-import './Colaborador.css'
+import './Colaborador.css';
+import { AiFillCloseCircle } from 'react-icons/ai';
 
-
-const Colaborador = ({nome, cargo, imagem, corDeFundo}) => {
-    return ( <div className="colaborador">
-        <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
-            <img src={imagem} alt={nome} />
+const Colaborador = ({ id, nome, imagem, cargo, corDeFundo, aoDeletar }) => {
+    return (
+        <div className='colaborador'>
+            <AiFillCloseCircle
+                size={25}
+                className="deletar"
+                onClick={() => aoDeletar(id)}
+                title="Deletar colaborador"
+            />
+            <div className='cabecalho' style={{ backgroundColor: corDeFundo }}>
+                <img src={imagem} alt={nome} />
+            </div>
+            <div className='rodape'>
+                <h4>{nome}</h4>
+                <h5>{cargo}</h5>
+            </div>
         </div>
-        <div className="rodape">
-            <h4>{nome}</h4>
-            <h5>{cargo}</h5>
-        </div>
-    </div>
-
-    )
-}
+    );
+};
 
 export default Colaborador;
